40302431624db4c25bc312377d49e228bfe06212
x-pog <72897649+x-pog@users.noreply.github.com>
2022-02-01 10:30:02 -0800

    main window change

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+import pygame
 
+(width, height) = (900, 900)
+screen = pygame.display.set_mode((width, height))
+pygame.display.flip()
+
+running = True
+while running:
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      running = False
