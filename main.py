5676f1611809e5a369704368ccf39ccd65d5420b
YiLongMusk <98840500+YiLongMusk@users.noreply.github.com>
2022-03-08 08:49:35 -0800

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-import util
-
-startyear = int(input())
-period = int(input())
-period += 1
-ticks = int(input())
-
-util.write(filename = r"temp.txt", start = startyear, period = period)
-util.plot(filename = r"temp.txt", startyear = startyear, endyear = startyear + (period - 1), ticks = ticks)
